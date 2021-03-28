b801e3ae5d46338934a8b5e91d91cc8cbf690ece
YankaFunka <50418166+YankaFunka@users.noreply.github.com>
2021-03-28 22:43:56 +0300

    Add files via upload

---
+++ Creative agency/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#burger').addEventListener('click', function() {
+        document.querySelector('#burger-menu').classList.toggle('burger-menu')
+
+    });
+})
